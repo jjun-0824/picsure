490e31ee679df51a26a2295bce212f8c4ef50e46
jjun-0824 <jangij0824@gmail.com>
2023-12-03 14:51:35 +0900

    finalize

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,124 @@
 from flask import Flask, request, render_template, url_for,jsonify
 import os
+from ultralytics import YOLO
+import cv2
 import time
 import sys
 import requests
+import mod_dbconn
+import boto3
+from decouple import config
+
+AWS_ACCESS_KEY = config('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = config('AWS_SECRET_KEY')
 
 app = Flask(__name__)
+
+def load_yolo_model(model_path):
+    return YOLO(model=model_path, task='detect')
+
+def is_person(image_filepath, model):
+    results = model.predict(image_filepath)
+    is_person_detected = any(0 in list(result.boxes.cls) for result in results)
+    return is_person_detected
+
+
+def transform_entire_image(image_filepath):
+    image = cv2.imread(image_filepath)
+    x, y, w, h = 0, 0, image.shape[1], image.shape[0]
+    roi = image[y:y+h, x:x+w]
+    factor = 10
+    small_roi = cv2.resize(roi, (w // factor, h // factor))
+    mosaic_roi = cv2.resize(small_roi, (w, h), interpolation=cv2.INTER_NEAREST)
+    image[y:y+h, x:x+w] = mosaic_roi
+    return image
+
+
+def transform_faces(image_filepath, model):
+    image = cv2.imread("." + image_filepath)
+    results = model.predict(image_filepath)
+
+    for result in results:
+        boxes = result.boxes.xywh.tolist()
+
+        for box in boxes:
+            x, y, w, h = map(int, list(box))
+            roi = image[y-h:y+h, x-w:x+w]
+            factor = 20
+            small_roi = cv2.resize(roi, (2*w // factor, 2*h // factor))
+            mosaic_roi = cv2.resize(small_roi, (2*w, 2*h), interpolation=cv2.INTER_NEAREST)
+            image[y-h:y+h, x-w:x+w] = mosaic_roi
+
+    return image
+
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    file = request.files['file']
+    filename = file.filename
+    filepath = os.path.join('static/uploads', filename)
+    file.save(filepath)
+
+    yolo_model_person = load_yolo_model('yolov8n.pt')
+    yolo_model_face = load_yolo_model('yolov8n-face.pt')
+
+    if is_person(filepath, yolo_model_person):
+        transformed_image = transform_faces(filepath, yolo_model_face)
+    else:
+        transformed_image = transform_entire_image(filepath)
+
+    return {}
+
+#s3 버킷과 연동
+s3 = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_SECRET_KEY)
+
+#s3 test - image upload시 s3버킷에 업로드 확인, url 해당 image의 url인 것 확인
+#image upload : input type="file"을 통해 업로드 받음
+#s3버킷에 업로드 후 해당 image의 url 바로 db에 update
+@app.route('/imgupload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+
+    s3.upload_fileobj(file, 'myphotobuckettt', file.filename)
+
+    db_class = mod_dbconn.Database()
+    url = f"https://myphotobuckettt.s3.ap-northeast-2.amazonaws.com/{file.filename}"
+
+    sql = "UPDATE User SET p2 = %s WHERE user_id = 1"
+    db_class.execute(sql, (url,))
+    db_class.commit()
+
+    return f"https://myphotobuckettt.s3.ap-northeast-2.amazonaws.com/{file.filename}"
+
+#s3 test - url을 통해서 image가져오기
+#image 불러올 때 img src="url"로 불러오기 가능 확인
+#db에서 해당 image의 url을 가져와서 그 url을 img src로 사용하도록 html로 넘겨주는 방식 (<img src="{{ image_url }}" alt="Rendered Image">)
+@app.route('/getimg')
+def get_file():
+    db_class = mod_dbconn.Database()
+
+    sql = "SELECT p2 FROM app_db.User WHERE user_id = 1"
+    res = db_class.executeOne(sql)
+
+    if res and 'p2' in res:
+        url = res['p2']         #res에서 p2값 url을 추출
+
+        #return render_template('img.html', image_url=url)      #해당 url값을 img src로 사용할 수 있도록 image_url을 통해 넘겨줌 (test시)
+        return jsonify({'image_url': url})                      #해당 url값을 json 형식으로 return
+
+
+@app.route('/db')
+def select():
+    db_class = mod_dbconn.Database()
+
+    sql = "SELECT link_id, url, password FROM app_db.Link"
+    row = db_class.executeAll(sql)
+
+    print(row)
+
+    #return render_template('db.html', resultData=row[0])
+
+    return row
+
 
 @app.route('/')
 def index():
@@ -42,9 +156,10 @@ def fail():
 def success():
     # 기본적으로 이 index 함수가 실행됨.
     return render_template('success.html')
+
 @app.route('/link')
 def get_link():
-    link="https://1234"
+    link="https://"
     # 전달해주고 싶은 결과값을 html내에서 변경해주고 싶을 때 하단과 같은 방법으로 변환
     return render_template('home.html', url=link) # 넘겨주고 싶은 변수 옆에 적어넣기
 
@@ -75,12 +190,12 @@ def saveFavorA():
     if photo_num in [1, 2, 3] and favor in ['Yes', 'No']:
         resp_dict['user_a'][photo_num] = favor          #해당되는 photo_num과 그에 대한 user_a의 response를 dictionary에 저장
 
-    #서버측으로 POST요청 보내는 경우
-    # url = 'http://server_domain/saveFavorA'
-    # data = {'photo_num': photo_num, 'favor': favor}
+    # 서버측으로 POST요청 보내는 경우
+    url = 'http://server_domain/saveFavorA'
+    data = {'photo_num': photo_num, 'favor': favor}
 
-    # response = requests.post(url, data=data)            #저장된 data들과 함께 서버의 엔드포인트로 POST요청을 보냄
-    # print(response.text)
+    response = requests.post(url, data=data)            #저장된 data들과 함께 서버의 엔드포인트로 POST요청을 보냄
+    print(response.text)
     return {}
 
 @app.route('/getFavorA', methods=['GET'])               
@@ -112,13 +227,15 @@ def saveFavorB():
 
     if photo_num in [1, 2, 3] and favor in ['Yes', 'No']:
         resp_dict_2['user_b'][photo_num] = favor          #해당되는 photo_num과 그에 대한 user_a의 response를 dictionary에 저장
+        if photo_num in [1, 2, 3] and favor in ['Yes', 'No']:
+            resp_dict_2['user_a'][photo_num] = favor          #해당되는 photo_num과 그에 대한 user_a의 response를 dictionary에 저장
 
-    #서버측으로 POST요청 보내는 경우
-    # url = 'http://server_domain/saveFavorA'
-    # data = {'photo_num': photo_num, 'favor': favor}
+    # 서버측으로 POST요청 보내는 경우
+    url = 'http://server_domain/saveFavorA'
+    data = {'photo_num': photo_num, 'favor': favor}
 
-    # response = requests.post(url, data=data)            #저장된 data들과 함께 서버의 엔드포인트로 POST요청을 보냄
-    # print(response.text)
+    response = requests.post(url, data=data)            #저장된 data들과 함께 서버의 엔드포인트로 POST요청을 보냄
+    print(response.text)
     return {}
 
 @app.route('/getFavorB', methods=['GET'])               
@@ -135,4 +252,4 @@ def getFavorB():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ backend/mod_dbconn.py
@@ -0,0 +1,30 @@
+import pymysql
+from decouple import config
+
+DB_HOST = config('DB_HOST')
+DB_USER = config('DB_USER')
+DB_PASSWORD = config('DB_PASSWORD')
+DB_NAME = config('DB_NAME')
+
+class Database():
+    def __init__(self):
+        #self.db = pymysql.connect(host='database-1.cqlgpwh0yz60.ap-northeast-2.rds.amazonaws.com', user='root', password='jun147147', db='app_db', charset='utf8')
+        self.db = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_NAME, charset='utf8')
+
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+
+    def execute(self, query, args={}):
+        self.cursor.execute(query, args)
+
+    def executeOne(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchone()
+        return row
+    
+    def executeAll(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchall()
+        return row
+    
+    def commit(self):
+        self.db.commit()
